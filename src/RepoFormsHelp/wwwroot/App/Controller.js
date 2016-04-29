ab660bf0d90fa2d77db91aa1bc0b49a9a14bf8c2
ET Mbpro <eric.n.tran@gmail.com>
2016-04-29 08:53:01 -0700

    Limit address lookup to USA and fixed calendar popups..

--- src/RepoFormsHelp/wwwroot/App/Controller.js
+++ src/RepoFormsHelp/wwwroot/App/Controller.js
@@ -17,7 +17,10 @@ app.controller('repoCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.getLocation = function (val) {
         return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
             params: {
-                address: val,
+                address: val + ', USA',
+                //componentRestrictions: {
+                //    country: 'US'
+                //},
                 sensor: false
             }
         }).then(function (response) {
@@ -27,24 +30,38 @@ app.controller('repoCtrl', ['$scope', '$http', function ($scope, $http) {
         });
     };
 
-    $scope.onSelect = function ($item) {
+    $scope.onSelect = function ($item, $type) {
+
+        // This is to cover the different address fields
         var item = $item;
 
         var street = '';
         var stnumber = '';
-        for (var ac = 0; ac < item.address_components.length; ac++) {
-            if (item.address_components[ac].types[0] === "street_number") { stnumber = item.address_components[ac].long_name }
-            if (item.address_components[ac].types[0] === "route") { street = item.address_components[ac].short_name }
-            if (item.address_components[ac].types[0] === "locality") { $scope.rf.storageCity = item.address_components[ac].long_name }
-            if (item.address_components[ac].types[0] === "administrative_area_level_1") { $scope.rf.storageState = item.address_components[ac].short_name }
-            if (item.address_components[ac].types[0] === "postal_code") { $scope.rf.storageZip = item.address_components[ac].long_name }
-        }
+        var ac = 0;
 
-        if (stnumber !== null && street !== null) {
-            $scope.rf.storageAddress = (stnumber + ' ' + street).trim();
-            
+        if ($type === 'storage') {
+            for (ac = 0; ac < item.address_components.length; ac++) {
+                if (item.address_components[ac].types[0] === "street_number") { stnumber = item.address_components[ac].long_name }
+                if (item.address_components[ac].types[0] === "route") { street = item.address_components[ac].short_name }
+                if (item.address_components[ac].types[0] === "locality") { $scope.rf.storageCity = item.address_components[ac].long_name }
+                if (item.address_components[ac].types[0] === "administrative_area_level_1") { $scope.rf.storageState = item.address_components[ac].short_name }
+                if (item.address_components[ac].types[0] === "postal_code") { $scope.rf.storageZip = item.address_components[ac].long_name }
+            }
+            if (stnumber !== null && street !== null) {
+                $scope.rf.storageAddress = (stnumber + ' ' + street).trim();
+            }
+        } else if ($type === 'recovery') {
+            for (ac = 0; ac < item.address_components.length; ac++) {
+                if (item.address_components[ac].types[0] === "street_number") { stnumber = item.address_components[ac].long_name }
+                if (item.address_components[ac].types[0] === "route") { street = item.address_components[ac].short_name }
+                if (item.address_components[ac].types[0] === "locality") { $scope.rf.recoveryCity = item.address_components[ac].long_name }
+                if (item.address_components[ac].types[0] === "administrative_area_level_1") { $scope.rf.recoveryState = item.address_components[ac].short_name }
+                if (item.address_components[ac].types[0] === "postal_code") { $scope.rf.recoveryZip = item.address_components[ac].long_name }
+            }
+            if (stnumber !== null && street !== null) {
+                $scope.rf.recoveryAddress = (stnumber + ' ' + street).trim();
+            }
         }
-
     };
 
     $scope.submitted = false;
@@ -72,28 +89,39 @@ app.controller('repoCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.format = $scope.formats[0];
     $scope.altInputFormats = ['M!/d!/yyyy'];
 
+    $scope.enumPopupType = {
+        CREATED: 1,
+        REPO: 2,
+        SIGNED: 3
+    }
+
+
     $scope.today = function () {
         $scope.rf.createdDate = new Date();
         $scope.rf.repoDate = new Date();
     };
     $scope.today();
 
-    $scope.openCreateDatePopup = function () {
-        $scope.createdDatePopup.opened = true;
+    $scope.openDatePopup = function (popup) {
+        switch (popup) {
+            case $scope.enumPopupType.CREATED:
+                $scope.datePopupStatus.created = true;
+                break;
+            case $scope.enumPopupType.REPO:
+                $scope.datePopupStatus.repo = true;
+                break;
+            case $scope.enumPopupType.SIGNED:
+                $scope.datePopupStatus.signed = true;
+                break;
+            default:
+        }
     };
 
-    $scope.createdDatePopup = {
-        opened: false
+    $scope.datePopupStatus = {
+        created: false,
+        repo: false,
+        signed: false
     };
-
-    $scope.openRepoDatePopup = function () {
-        $scope.repoDatePopup.opened = true;
-    };
-
-    $scope.repoDatePopup = {
-        opened: false
-    };
-
 
 }]);
 
